0354e276063e71de5b68da25cf8f53f9916a6803
Jason Liu <jason.liu@oocl.com>
2018-11-21 10:25:43 +0800

    renamed variable

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 
 
-function createMultiplicationTable(num1, num2){
-	if(!checkifNuminRange(num1, num2)) return null;
-	if(!checkifStartNumSmaller(num1, num2)) return null;
+function createMultiplicationTable(startNum, endNum){
+	if(!checkifNuminRange(startNum, endNum)) return null;
+	if(!checkifStartNumSmaller(startNum, endNum)) return null;
 	var str = "";
-	for (var i = num1 ; i <= num2 ; i++){
-		for (var j = num1 ; j <= i ; j++){
-			str = str + appendTableStr(j,i,num2);
+	for (var i = startNum ; i <= endNum ; i++){
+		for (var j = startNum ; j <= i ; j++){
+			str = str + appendTableStr(j,i,endNum);
 		}
-		if (i!=num2)
+		if (i!=endNum)
 			str = str + "\n";
 	}
 	return str;
 }
-function checkifNuminRange(num1, num2){
-	if (num1 > 1000 || num1 < 1 || num2 > 1000 || num2 < 1){
+function checkifNuminRange(startNum, endNum){
+	if (startNum > 1000 || startNum < 1 || endNum > 1000 || endNum < 1){
 		return false;
 	}
 	return true;
 }
-function checkifStartNumSmaller(num1, num2){
-	if (num1 > num2){
+function checkifStartNumSmaller(startNum, endNum){
+	if (startNum > endNum){
 		return false;
 	}
 	return true;
 }
-function appendTableStr(num1, num2, max){
-	var line = num1.toString()+"*"+num2.toString()+"="+num1*num2;
-	if (num1!=num2)
+function appendTableStr(startNum, endNum, max){
+	var line = startNum.toString()+"*"+endNum.toString()+"="+startNum*endNum;
+	if (startNum!=endNum)
 		return alignTable(line, (max*max).toString().length+max.toString().length+max.toString().length+2);
-	return num1.toString()+"*"+num2.toString()+"="+num1*num2;
+	return startNum.toString()+"*"+endNum.toString()+"="+startNum*endNum;
 }
 function alignTable(line, maxlength){
 	var appendlength = maxlength - line.length;
